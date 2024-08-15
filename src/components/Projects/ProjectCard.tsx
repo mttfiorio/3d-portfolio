97ee97c5b9f80e7825ee0ca1f4a40b3afbad9d27
mattia fiorio <mattia.fiorio@akqa.com>
2024-08-15 11:18:37 +0200

    Adjust Project cards

--- src/components/Projects/ProjectCard.tsx
+++ src/components/Projects/ProjectCard.tsx
@@ -1,5 +1,4 @@
-import React, { useRef, useEffect, ReactNode } from "react";
-import Tilt from "react-parallax-tilt";
+import React, { useRef, ReactNode } from "react";
 import { motion } from "framer-motion";
 import { fadeIn } from "../../utils/motion";
 
@@ -20,42 +19,26 @@ interface ProjectCardProps {
 
 interface VideoWrapper {
   videoSrc: string;
-  children: ReactNode;
+  children?: ReactNode;
 }
 
-const VideoWrapper = ({ videoSrc, children }: VideoWrapper) => {
+const VideoWrapper = ({ videoSrc }: VideoWrapper) => {
   const videoRef = useRef<HTMLVideoElement>(null);
 
-  useEffect(() => {
-    const handleIntersection = ([entry]: IntersectionObserverEntry[]) => {
-      if (entry.isIntersecting) {
-        videoRef.current?.play();
-      } else {
-        videoRef.current?.pause();
-      }
-    };
-
-    const observer = new IntersectionObserver(handleIntersection, {
-      threshold: 0.8,
-    });
-    videoRef.current && observer.observe(videoRef.current);
-
-    return () => observer.disconnect();
-  }, []);
-
   return (
-    <div className="relative rounded-2xl overflow-hidden diagonal-cut-mirrored">
-      <video
-        ref={videoRef}
-        muted
-        loop
-        className="absolute top-0 left-0 w-full h-full object-cover z-0"
-      >
+    <motion.div
+      initial={{ opacity: 0 }} // Initial state of opacity
+      whileHover={{ opacity: 1 }} // State of opacity while hovering
+      className="absolute top-0 left-0 w-full h-full z-10"
+      onHoverStart={() => videoRef.current?.play()}
+      onHoverEnd={() => videoRef.current?.pause()}
+    >
+      <div className="absolute top-0 left-0 w-full h-full bg-dark/10" />
+      <video muted loop ref={videoRef} className="w-full h-full object-cover">
         <source src={"projects/" + videoSrc} type="video/mp4" />
         Your browser does not support the video tag.
       </video>
-      <div className="relative z-10 bg-dark/80">{children}</div>
-    </div>
+    </motion.div>
   );
 };
 
@@ -69,32 +52,41 @@ const ProjectCard: React.FC<ProjectCardProps> = ({
   video,
 }) => {
   return (
-    <motion.div variants={fadeIn("up", "spring", index * 0.5, 0.75)}>
-      <Tilt
-        tiltMaxAngleX={-20}
-        tiltMaxAngleY={-20}
-        scale={1}
-        transitionSpeed={450}
-        className="xs:w-[360px] w-full "
+    <motion.a
+      href={link}
+      aria-label={name + "-link"}
+      variants={fadeIn("up", "spring", index * 0.5, 0.75)}
+      className="relative sm:w-[360px] min-h-[400px] w-full"
+    >
+      <div
+        className="absolute w-full h-full left-1 top-1 -z-10
+       bg-white rounded-2xl diagonal-cut-mirrored"
+      />
+
+      <div
+        className="absolute w-full h-full
+      rounded-2xl overflow-hidden diagonal-cut-mirrored "
       >
-        <a href={link} aria-label={name + "-link"}>
-          <div className="absolute w-full h-full bg-white rounded-2xl left-1 top-1 -z-10 diagonal-cut-mirrored" />
-          <VideoWrapper videoSrc={video}>
-            <div className="p-6 flex flex-col gap-4">
-              <h3 className="text-white">{name}</h3>
-              <p className="text-white">{description}</p>
-              <div className="mt-4 flex flex-wrap justify-end gap-2">
-                {tags.map((tag) => (
-                  <p key={`${name}-${tag.name}`} className={`${tag.color}`}>
-                    #{tag.name}
-                  </p>
-                ))}
-              </div>
-            </div>
-          </VideoWrapper>
-        </a>
-      </Tilt>
-    </motion.div>
+        <div className="relative p-6 w-full h-full flex flex-col justify-center gap-4 z-10 bg-dark/90">
+          <h3 className="text-white">{name}</h3>
+          <p className="text-white">{description}</p>
+          <div className="mt-4 flex flex-wrap justify-end gap-2">
+            {tags.map((tag) => (
+              <p key={`${name}-${tag.name}`} className={`${tag.color}`}>
+                #{tag.name}
+              </p>
+            ))}
+          </div>
+        </div>
+
+        <img
+          src={"projects/" + image}
+          className="absolute top-0 left-0 w-full h-full object-cover z-0"
+        />
+
+        <VideoWrapper videoSrc={video} />
+      </div>
+    </motion.a>
   );
 };
 
